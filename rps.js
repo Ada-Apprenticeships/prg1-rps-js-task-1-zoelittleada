97fc55c21e2904c414f9d8c57db5b2bebdcfcbf1
zoelittleada <zoe.little@ada.ac.uk>
2024-11-26 11:21:37 +0000

    final commit

--- rps.js
+++ rps.js
@@ -1,31 +1,23 @@
-const readlineSync = require('readline-sync');
 function rockPaperScissors(player1, player2) {
-  //While loops ask for user input until rock, paper or scissors is inputted
-  let rps = ['rock', 'paper','scissors']
-  player1 = readlineSync.question ('Player 1, choose rock, paper or scissors: ');
-  while (!rps.includes(player1)){
-    player1 = readlineSync.question ('Please enter rock, paper or scissors to continue: ');
-  }
-  player2 = readlineSync.question ('Player 2, choose rock, paper or scissors: ');
-  while (!rps.includes(player2)){
-    player2 = readlineSync.question ('Please enter rock, paper or scissors to continue: ');
-  }
   
-  if (player1 === player2){
-    return 'Draw'
-  } else if ((player1==='rock'&&player2==='scissors')||(player1==='scissors'&&player2==='paper'||player1==='paper'&&player2==='rock')){
-    return 'Player1 Wins'
-  } else {
-    return 'Player2 wins'
-  }
+  // Object to define the winning rules for each choice
+  const winning = {
+    rock: ["lizard", "scissors"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    spock: ["scissors", "rock"],
+    lizard: ["spock", "paper"],
+  };
+  
+  // Checks for draw (both players choose the same option)
+  if (player1 === player2) {return "draw";}
+  
+  // Check if player2 is included in the winning rules for player1
+  return winning[player1].includes(player2)
+  ? "player1" // If player2 is in the array that player1 beats, player1 wins
+  : "player2"; // Othwerwise player2 wins
 }
-
-console.log(rockPaperScissors())
-
-
-
-
-
+ 
 
 
 
@@ -34,4 +26,4 @@ console.log(rockPaperScissors())
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
-}
+};
